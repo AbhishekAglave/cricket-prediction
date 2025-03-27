fe50d61b52a89b06ed04c418c9ddd3634324b9df
Abhishek Aglave <abhishekaglave@192.168.1.35>
2025-03-27 13:11:49 +0530

    redirection issue fix

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       </div>
 
       <div className="flex justify-center">
-        <Link href={'/contact-us'} className="mb-4 py-2 px-4 gradient-button">
+        <Link href={'/welcome'} className="mb-4 py-2 px-4 gradient-button">
           <span>Chat With Us</span>
           <span className="ml-1 animate-wiggle-horizontal">
             <ChevronRightIcon className="h-[24px] w-[24px]" />
@@ -59,7 +59,7 @@ export default function Home() {
       {/* Get in Touch */}
       <h3 className="text-2xl md:text-3xl font-semibold text-center mb-4">Get in touch</h3>
       <div className="flex justify-center">
-        <Link href={'/contact-us'} className="mb-4 py-2 px-4 gradient-button">
+        <Link href={'/welcome'} className="mb-4 py-2 px-4 gradient-button">
           <span>Chat With Us</span>
           <span className="ml-1 animate-wiggle-horizontal">
             <ChevronRightIcon className="h-[24px] w-[24px]" />
